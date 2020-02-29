60f68d344a43c38af1bcbba4eba005b5b6e59859
MikeAlpaXRay <61335415+MikeAlpaXRay@users.noreply.github.com>
2020-02-29 04:30:58 +0100

    add Autocomplete function

--- functions.py
+++ functions.py
@@ -1,6 +1,8 @@
 import requests
 import datetime
 import json
+import re
+import tkinter as tk
 
 DEFAULT_MMR = 2000
 
@@ -63,6 +65,84 @@ class Player:
                 return int(player_mmr / count)
 
 
+class AutocompleteEntry(tk.Entry):
+    def __init__(self, player_name_list, *args, **kwargs):
+
+        tk.Entry.__init__(self, *args, **kwargs)
+        self.player_name_list = player_name_list
+        self.var = self["textvariable"]
+        if self.var == '':
+            self.var = self["textvariable"] = tk.StringVar()
+
+        self.var.trace('w', self.changed)
+        self.bind("<Right>", self.selection)
+        self.bind("<Up>", self.up)
+        self.bind("<Down>", self.down)
+
+        self.lb_up = False
+
+    def changed(self, name, index, mode):
+
+        if self.var.get() == '':
+            self.lb.destroy()
+            self.lb_up = False
+        else:
+            words = self.comparison()
+            if words:
+                if not self.lb_up:
+                    self.lb = tk.Listbox()
+                    self.lb.bind("<Double-Button-1>", self.selection)
+                    self.lb.bind("<Right>", self.selection)
+                    self.lb.place(x=self.winfo_x(), y=self.winfo_y() + self.winfo_height())
+                    self.lb_up = True
+
+                self.lb.delete(0, tk.END)
+                for w in words:
+                    self.lb.insert(tk.END, w)
+            else:
+                if self.lb_up:
+                    self.lb.destroy()
+                    self.lb_up = False
+
+    def selection(self, event):
+
+        if self.lb_up:
+            self.var.set(self.lb.get(tk.ACTIVE))
+            self.lb.destroy()
+            self.lb_up = False
+            self.icursor(tk.END)
+
+    def up(self, event):
+
+        if self.lb_up:
+            if self.lb.curselection() == ():
+                index = '0'
+            else:
+                index = self.lb.curselection()[0]
+            if index != '0':
+                self.lb.selection_clear(first=index)
+                index = str(int(index) - 1)
+                self.lb.selection_set(first=index)
+                self.lb.activate(index)
+
+    def down(self, event):
+
+        if self.lb_up:
+            if self.lb.curselection() == ():
+                index = '0'
+            else:
+                index = self.lb.curselection()[0]
+            if index != tk.END:
+                self.lb.selection_clear(first=index)
+                index = str(int(index) + 1)
+                self.lb.selection_set(first=index)
+                self.lb.activate(index)
+
+    def comparison(self):
+        pattern = re.compile('.*' + self.var.get() + '.*')
+        return [w for w in self.player_name_list if re.match(pattern, w)]
+
+
 def load_json():
     players = []
     with open("player_data\\player_data.json", 'r') as json_file:
@@ -80,6 +160,7 @@ def save_json(player_list):
 
 
 if __name__ == "__main__":
+    print()
 # try:
 #     player_list = load_json()
 #     player_list.append(Player("TheBiche."))
